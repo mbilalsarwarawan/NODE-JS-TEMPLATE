36cebb7945391ea8e7a8ba53f1898fe12e44bdbd
BilalAwancherrbyte <bilal@thecherrybyte.com>
2025-04-16 15:15:50 +0500

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import routes from './routes/index.js';
+import connectDB from './config/db.js';
+import requestLogger from './middlewares/requestLogger.js';
+import responseLogger from './middlewares/responseLogger.js';
+import errorHandler from './middlewares/errorHandler.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(requestLogger);
+app.use(responseLogger);
+
+app.use('/api', routes);
+
+app.use(errorHandler);
+
+export default app;
